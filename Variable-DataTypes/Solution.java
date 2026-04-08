4013a3fc67a5fb0982572e9e341ef071445ded30
Faisal-Khan433 <khanfaisal15689@gmail.com>
2026-04-08 11:02:51 +0530

    Add new Folder & files

---
+++ Variable-DataTypes/Solution.java
@@ -0,0 +1,56 @@
+/*
+public class Solution{
+    public static void main(String[] args){  // Multi-line comment
+        System.out.print("Hello World");
+
+    }
+}
+*/
+
+// public class Solution{
+//     public static void main(String[] args){    // Single-line comment
+//         System.out.println("*****");     
+//         System.out.println("****");
+//         System.out.println("***");
+//         System.out.println("**");
+//         System.out.println("*");
+//     }
+// }
+
+
+// public class Solution{
+//     public static void main(String[] args){  //simple sum of two numbers
+//         int a=1 , b=2; 
+//         int sum= a+b;
+//         System.out.print("Sum is :" + sum);
+
+//     }
+// }
+
+
+// public class Solution{
+//     public static void main(String[] args){  //simple subtraction of two numbers
+//         int a=4 , b=3; 
+//         int sub= a-b;
+//         System.out.print("Sub is :" + sub);
+
+//     }
+// }
+
+// public class Solution{
+//     public static void main(String[] args){  //simple product of two numbers
+//         int a=5 , b=6; 
+//         int product= a*b;
+//         System.out.print("Product is :" + product);
+
+//     }
+// }
+
+// public class Solution{
+//     public static void main(String[] args){  //simple Division of two numbers
+//         int a=10 , b=2; 
+//         int Division= a/b;
+//         System.out.print("Division is :" + Division);
+
+//     }
+// }
