3f33372fa6c4a1b95eca1b025d692a70b56ff2d2
Faisal-Khan433 <khanfaisal15689@gmail.com>
2026-04-09 11:07:32 +0530

    create Structure of DSA-Profile

---
+++ Aprail-8/Variable-DataTypes/Solution.java
@@ -0,0 +1,136 @@
+/*
+public class Solution{
+    public static void main(String[] args){  // Multi-line comment
+        System.out.print("Hello World");
+
+    }
+}
+*/
+
+// public class Solution{
+//     public static void main(String[] args){    // Single-line comment
+//         System.out.println("*****");     
+//         System.out.println("****");
+//         System.out.println("***");
+//         System.out.println("**");
+//         System.out.println("*");
+//     }
+// }
+
+
+// public class Solution{
+//     public static void main(String[] args){  //simple sum of two numbers
+//         int a=1 , b=2; 
+//         int sum= a+b;
+//         System.out.print("Sum is :" + sum);
+
+//     }
+// }
+
+
+// public class Solution{
+//     public static void main(String[] args){  //simple subtraction of two numbers
+//         int a=4 , b=3; 
+//         int sub= a-b;
+//         System.out.print("Sub is :" + sub);
+
+//     }
+// }
+
+// public class Solution{
+//     public static void main(String[] args){  //simple product of two numbers
+//         int a=5 , b=6; 
+//         int product= a*b;
+//         System.out.print("Product is :" + product);
+
+//     }
+// }
+
+// public class Solution{
+//     public static void main(String[] args){  //simple Division of two numbers
+//         int a=10 , b=2; 
+//         int Division= a/b;
+//         System.out.print("Division is :" + Division);
+
+//     }
+// }
+
+// import java.util.*;
+// public class Solution{
+//     public static void main(String[] args){     //Take the Input from User using Scannr class.
+//         System.out.println("Take the Input from User");
+//         System.out.println("Enter Your Name");
+//         Scanner sc=new Scanner(System.in);
+//         String userName= sc.nextLine();
+//         System.out.println("You are Welcome : " + userName);
+//     }
+// }
+
+// import java.util.*;
+// public class Solution{
+//     public static void main(String[] args){
+//         System.out.println("Take the Input from User");     //sum of two no using Scanner class
+//         System.out.println("Enter first number");
+//         Scanner sc=new Scanner(System.in);
+//         int a= sc.nextInt();
+//         System.out.println("Enter second number");
+//         int b= sc.nextInt();
+//         int sum = a+b;
+//         System.out.println("Sum of a & b is  : " + sum);
+//     }
+// }
+
+// import java.util.*;
+// public class Solution{    //subtraction of two no using Scanner class
+//     public static void main(String[] args){
+//         System.out.println("Take the Input from User"); 
+//         System.out.println("Enter first number");
+//         Scanner sc=new Scanner(System.in);
+//         int a= sc.nextInt();
+//         System.out.println("Enter second number");
+//         int b= sc.nextInt();
+//         int sub = a-b;
+//         System.out.println("Subtraction  of a & b is  : " + sub);
+//     }
+// }
+
+// import java.util.*;
+// public class Solution{  //Product of two no using Scanner class
+//     public static void main(String[] args){
+//         System.out.println("Take the Input from User");
+//         System.out.println("Enter first number");
+//         Scanner sc=new Scanner(System.in);
+//         int a= sc.nextInt();
+//         System.out.println("Enter second number");
+//         int b= sc.nextInt();
+//         int product = a*b;
+//         System.out.println("Subtraction  of a & b is  : " + product);
+//     }
+// }
+
+// import java.util.*;
+// public class Solution{    //Division of two no using Scanner class
+//     public static void main(String[] args){
+//         System.out.println("Take the Input from User");
+//         System.out.println("Enter first number");
+//         Scanner sc=new Scanner(System.in);
+//         int a= sc.nextInt();
+//         System.out.println("Enter second number");
+//         int b= sc.nextInt();
+//         int divid = a/b;
+//         System.out.println("Division  of a & b is  : " + divid);
+//     }
+// }
+
+// import java.util.*;
+// public class Solution{  //Calculate the Area of circle using Scanner class
+//     public static void main(String[] args){
+//         System.out.println("Calculate the Area of circle");
+//         System.out.println("Enter Radious");
+//         Scanner sc=new Scanner(System.in);
+//         int rad= sc.nextInt();
+//         float pi = (float)3.14;
+//         float area = (float)pi*rad*rad;
+//         System.out.println("Area of circle is : " + area);
+//     }
+// }
